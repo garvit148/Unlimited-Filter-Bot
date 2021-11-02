55fdafa959d647bb0183776ef1234fbb916ea8a8
garvit148 <61928763+garvit148@users.noreply.github.com>
2021-11-02 19:19:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
+I'm Black Sovereign with many capabilities!
 There is no practical limits for my filtering capacity :)
 
 See <i>/help</i> for commands and more details.</b>
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @gtxprime</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Black Sovereign</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @gtxprime    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
